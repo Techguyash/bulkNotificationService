d22de50cda3c72aaac0ad09317928be00f270906
techguyah <techguyash@gmail.com>
2022-02-07 21:28:48 +0530

    Status controller

--- src/main/java/com/notification/backend/bulkNotificationService/controller/Controller.java
+++ src/main/java/com/notification/backend/bulkNotificationService/controller/Controller.java
@@ -5,10 +5,7 @@ import com.notification.backend.bulkNotificationService.entity.Email;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController(value = "/app/api")
 public class Controller
@@ -16,13 +13,23 @@ public class Controller
     @Autowired
     private EmailService emailService;
 
+    @GetMapping("/status")
+    public ResponseEntity<String> statusCheck()
+    {
+        return new ResponseEntity<>("Up and running", HttpStatus.OK);
+    }
+
     @PostMapping("/mail")
     public ResponseEntity emailRequest(@RequestBody Email content)
     {
         boolean send = emailService.send(content);
-        if(send)
+        if (send)
+        {
             return new ResponseEntity(HttpStatus.OK);
+        }
         else
-            return new ResponseEntity("Send Successfully",HttpStatus.EXPECTATION_FAILED);
+        {
+            return new ResponseEntity("Send Successfully", HttpStatus.EXPECTATION_FAILED);
+        }
     }
 }
